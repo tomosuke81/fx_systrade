2ad4d0c5f3c68a4fea070ad646ec2fe154a2aa66
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-03-20 20:44:51 +0900

    removed OneDeviceStrategy use. specified l2 regularizer(0.01) to kernel of LSTM layers. changed optimizer from Adam to SGD.

--- dqn_fx_trade_tensorflow_lstm.py
+++ dqn_fx_trade_tensorflow_lstm.py
@@ -39,8 +39,8 @@ class QNetwork:
     def __init__(self, learning_rate=0.001, state_size=15, action_size=3, time_series=32):
         global all_period_reward_arr
 
-        self.optimizer = Adam(lr=learning_rate, clipvalue=5.0)
-        # self.optimizer = SGD(lr=learning_rate, momentum=0.9, clipvalue=5.0)
+        #self.optimizer = Adam(lr=learning_rate, clipvalue=5.0)
+        self.optimizer = SGD(lr=learning_rate, momentum=0.9, clipvalue=5.0)
         self.loss_func = tf.keras.losses.Huber(delta=1.0)
 
         if IS_TF_STYLE:
@@ -59,19 +59,18 @@ class QNetwork:
                 self.model.compile(optimizer=self.optimizer, loss=self.loss_func)
             else:
                 # 面倒なので動かすルートだけ、DropoutとBatchNormalizationの位置変えの修正を入れる（0320-1013）
-                strategy = tf.distribute.OneDeviceStrategy(device="/cpu:0")
-
-                with strategy.scope():
-                    self.model = tf.keras.Sequential([
-                        LSTM(hidden_size, input_shape=(time_series, state_size), return_sequences=True, activation=None, recurrent_dropout=0.5),
-                        LeakyReLU(0.2),
-                        Dropout(0.5),
-                        BatchNormalization(),
-                        LSTM(hidden_size, return_sequences=False, activation=None, recurrent_dropout=0.5),
-                        LeakyReLU(0.2),
-                        Dense(action_size, activation='linear')
-                    ])
-                    self.model.compile(optimizer=self.optimizer, loss=self.loss_func)
+                # strategy = tf.distribute.OneDeviceStrategy(device="/cpu:0")
+                # with strategy.scope():
+                self.model = tf.keras.Sequential([
+                    LSTM(hidden_size, input_shape=(time_series, state_size), return_sequences=True, activation=None, kernel_regularizer=l2(0.01), recurrent_dropout=0.5),
+                    LeakyReLU(0.2),
+                    Dropout(0.5),
+                    BatchNormalization(),
+                    LSTM(hidden_size, return_sequences=False, activation=None, kernel_regularizer=l2(0.01), recurrent_dropout=0.5),
+                    LeakyReLU(0.2),
+                    Dense(action_size, activation='linear')
+                ])
+                self.model.compile(optimizer=self.optimizer, loss=self.loss_func)
         else:
             self.model = Sequential()
 
