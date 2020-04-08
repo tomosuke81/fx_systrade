4f786b8803478fd0ff38a4c602d1b93c412f1f9b
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-04-08 19:36:56 +0900

    index 65 (2).

--- dqn_fx_trade_tensorflow_lstm.py
+++ dqn_fx_trade_tensorflow_lstm.py
@@ -338,6 +338,7 @@ def tarin_agent():
     for cur_itr in range(iteration_num):
         # 定期的にバックテストを行い評価できるようにしておく（CSVを吐く）
         if cur_itr % BACKTEST_ITR_PERIOD == 0 and cur_itr != 0:
+            mainQN.save_model("mainQN")
             run_backtest("auto_backtest", learingQN=mainQN, env_master=env_master)
             run_backtest("auto_backtest_test", learingQN=mainQN, env_master=env_master)
             continue
@@ -351,14 +352,14 @@ def tarin_agent():
         # ここだけ 同じstateから同じstateに遷移したことにする
         store_episode_log_to_memory(state, action, reward, state, info)
 
-        # スナップショットをとっておく
-        if cur_itr % 20 == 0 and cur_itr != 0:
-            mainQN.save_model("mainQN")
-            # memory.save_memory("memory")
-            # with open("./total_get_action_count.pickle", 'wb') as f:
-            #     pickle.dump(total_get_acton_cnt, f)
-            # with open("./all_period_reward_arr.pickle", 'wb') as f:
-            #     pickle.dump(all_period_reward_arr, f)
+        # # スナップショットをとっておく
+        # if cur_itr % 20 == 0 and cur_itr != 0:
+        #     mainQN.save_model("mainQN")
+        #     # memory.save_memory("memory")
+        #     # with open("./total_get_action_count.pickle", 'wb') as f:
+        #     #     pickle.dump(total_get_acton_cnt, f)
+        #     # with open("./all_period_reward_arr.pickle", 'wb') as f:
+        #     #     pickle.dump(all_period_reward_arr, f)
 
         # replay呼び出しに用いる（上ですでに一回行っているので1からスタート）
         total_episode_on_last_itr = 1
@@ -441,10 +442,11 @@ def run_backtest(backtest_type, learingQN=None, env_master=None):
     mainQN = QNetwork(learning_rate=learning_rate, time_series=time_series)     # メインのQネットワーク
     actor = Actor()
 
-    if backtest_type == "auto_backtest" or backtest_type == "auto_backtest_test":
-        mainQN = learingQN
-    else:
-        mainQN.load_model("mainQN")
+    mainQN.load_model("mainQN")
+    # if backtest_type == "auto_backtest" or backtest_type == "auto_backtest_test":
+    #     mainQN = learingQN
+    # else:
+    #     mainQN.load_model("mainQN")
 
     # DONOT でスタート
     state, reward, done, info, needclose = env.step(0)
