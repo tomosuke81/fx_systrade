ec605d4a678aec285d75bf194cd522ba7fd7df92
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-04-18 12:02:51 +0900

    index 95

--- thesis_based_dqn_trade_agent.py
+++ thesis_based_dqn_trade_agent.py
@@ -39,7 +39,7 @@ class QNetwork:
 
         inputlayer = Input(shape=(time_series, state_size))
         #middlelayer = LSTM(hidden_size_lstm1, return_sequences=True, activation=None)(inputlayer)
-        middlelayer = LSTM(hidden_size_lstm1, return_sequences=False, recurrent_dropout=0.5, activation=None)(inputlayer)
+        middlelayer = LSTM(hidden_size_lstm1, return_sequences=False, activation=None)(inputlayer)
         middlelayer = LeakyReLU(0.2)(middlelayer)
         # middlelayer = LSTM(hidden_size_lstm2, return_sequences=False, activation=None)(middlelayer)
         # middlelayer = LeakyReLU(0.2)(middlelayer)
@@ -112,14 +112,15 @@ class QNetwork:
                 next_action = -99 # Q関数の更新式を用いなくなった場合のため
 
                 target = reward_b
-                # 15itr以降は Q関数の更新式は用いず reward_b にそのまま fit させる
-                if cur_itr < 15:
-                    # Double DQN (mainQNとtargetQNを用いる。 Fixed Q-targetsもこれでおそらく実現できているのではないかと思われる)
-                    reshaped_next_state = np.reshape(next_state_b, [1, time_series, feature_num])
-                    retmainQs = self.model.predict(reshaped_next_state)[0]
-                    next_action = np.argmax(retmainQs)  # 最大の報酬を返す行動を選択する
-                    predicted_targetQN = targetQN.model.predict(reshaped_next_state)
-                    target = reward_b + gamma * predicted_targetQN[0][next_action]
+                
+                # # 15itr以降は Q関数の更新式は用いず reward_b にそのまま fit させる
+                # if cur_itr < 15:
+                # Double DQN (mainQNとtargetQNを用いる。 Fixed Q-targetsもこれでおそらく実現できているのではないかと思われる)
+                reshaped_next_state = np.reshape(next_state_b, [1, time_series, feature_num])
+                retmainQs = self.model.predict(reshaped_next_state)[0]
+                next_action = np.argmax(retmainQs)  # 最大の報酬を返す行動を選択する
+                predicted_targetQN = targetQN.model.predict(reshaped_next_state)
+                target = reward_b + gamma * predicted_targetQN[0][next_action]
 
                 #action_conved = 0 if action_b == -1 else 1
                 action_conved = action_b + 1
